0727ea64cbeb5136bc44c7011d3bdce443aaf1d7
RimaSem <rima.semetaite@gmail.com>
2023-03-26 12:51:04 +0300

    Create initial main section

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="header">
-        <input type="color"></input>
+        <input type="color" value="#f55a5a"></input>
         <div className="select">
           <select>
             <option>Monochrome</option>
@@ -22,6 +22,23 @@ function App() {
 
         <button type="button">Get color scheme</button>
       </div>
+      <main>
+        <div className="color color-1">
+          <div className="color-code">#F55A5A</div>
+        </div>
+        <div className="color color-2">
+          <div className="color-code">#2B283A</div>
+        </div>
+        <div className="color color-3">
+          <div className="color-code">#FBF3AB</div>
+        </div>
+        <div className="color color-4">
+          <div className="color-code">#AAD1B6</div>
+        </div>
+        <div className="color color-5">
+          <div className="color-code">#A626D3</div>
+        </div>
+      </main>
     </div>
   );
 }
